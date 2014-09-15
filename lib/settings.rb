1406aae930442b116b3fa344a979bd435c26d2e2
Michal Kimle <kimle.michal@gmail.com>
2014-09-15 13:10:41 +0200

    fixed loading from configuration file when installed as gem

--- lib/settings.rb
+++ lib/settings.rb
@@ -7,7 +7,7 @@ class Settings < Settingslogic
     if File.exist?("#{ENV['HOME']}/.oneacct-export/#{CONF_NAME}")
   source "/etc/oneacct-export/#{CONF_NAME}"\
     if File.exist?("/etc/oneacct-export/#{CONF_NAME}")
-  source "config/#{CONF_NAME}"
+  source "#{File.dirname(__FILE__)}/../config/#{CONF_NAME}"
 
   namespace ENV['RAILS_ENV'] ? ENV['RAILS_ENV'] : 'production'
 end
