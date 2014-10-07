8f3282b1904e8dfae016e6d04f2020a6eb7adb08
Michal Kimle <kimle.michal@gmail.com>
2014-10-07 15:13:11 +0200

    removed development specific code

--- bin/oneacct-export.rb
+++ bin/oneacct-export.rb
@@ -1,6 +1,4 @@
 #!/usr/bin/env ruby
-lib_dir = "#{File.dirname(__FILE__)}/../lib"
-$LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 ENV['RAILS_ENV'] = ENV['RAILS_ENV'] ? ENV['RAILS_ENV'] : 'production'
 
 require 'syslogger'
