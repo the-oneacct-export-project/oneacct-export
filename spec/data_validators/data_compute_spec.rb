c2148cd3e1e8c0a773c24de9d2a7c9fd6919d0a7
Michal Kimle <kimle.michal@gmail.com>
2015-04-09 19:09:34 +0200

    added tests for modules DataCompute and DataValidatorHelper

--- spec/data_validators/data_compute_spec.rb
+++ spec/data_validators/data_compute_spec.rb
@@ -2,93 +2,178 @@ require 'spec_helper'
 
 module DataValidators
   describe DataCompute do
+    subject { data_compute }
+
+    let(:data_compute) do
+      data_compute = Object.new
+      data_compute.extend(Errors)
+      data_compute.extend(InputValidator)
+      data_compute.extend(DataValidatorHelper)
+      data_compute.extend(DataCompute)
+
+      data_compute
+    end
+
+    let(:log) { double('log') }
+
+    before :example do
+      allow(subject).to receive(:log).and_return(log)
+      allow(log).to receive(:warn)
+    end
 
     describe '.sum_disk_size' do
-      let(:vm) do
-        xml = File.read("#{GEM_DIR}/mock/#{filename}")
-        OpenNebula::XMLElement.new(OpenNebula::XMLElement.build_xml(xml, 'VM'))
+      let(:disks) do
+        disks = []
+        disk = {'size' => '10240'}
+        disks << disk
+        disk = {'size' => '42368'}
+        disks << disk
+
+        disks
       end
 
-      context 'vm with DISK without SIZE' do
-        let(:filename) { 'one_worker_valid_machine.xml' }
+      let(:vm_id) { 42 }
 
-        it 'returns NULL' do
-          expect(subject.sum_disk_size(vm)).to eq('NULL')
+      context 'vm without disks' do
+        it 'returns 0' do
+          expect(subject.sum_disk_size([], vm_id)).to eq(0)
         end
       end
 
-      context 'vm with DISK with invalid SIZE' do
-        let(:filename) { 'one_worker_DISK_SIZE_nan.xml' }
+      context 'vm with disk without size' do
+        before :example do
+          disks[0] = {}
+        end
 
-        it 'returns NULL' do
-          expect(subject.sum_disk_size(vm)).to eq('NULL')
+        it 'returns nil' do
+          expect(subject.sum_disk_size(disks, vm_id)).to be_nil
         end
       end
 
-      context 'vm with single DISK and valid SIZE' do
-        let(:filename) { 'one_worker_vm7.xml' }
+      context 'vm with disk with invalid size' do
+        before :example do
+          disks[0] = {'size' => 'not_a_number'}
+        end
 
+        it 'returns nil' do
+          expect(subject.sum_disk_size(disks, vm_id)).to be_nil
+        end
+      end
+
+      context 'vm with single disk and valid size' do
         it 'return correct disk size' do
-          expect(subject.sum_disk_size(vm)).to eq(11)
+          expect(subject.sum_disk_size(disks[1..1], vm_id)).to eq(42368)
         end
       end
 
-      context 'vm with multiple DISKs and valid SIZE' do
-        let(:filename) { 'one_worker_vm8.xml' }
-
+      context 'vm with multiple disks and valid size' do
         it 'return correct disk size' do
-          expect(subject.sum_disk_size(vm)).to eq(53)
+          expect(subject.sum_disk_size(disks, vm_id)).to eq(52608)
         end
       end
     end
 
     describe '.sum_rstime' do
-      let(:vm) do
-        xml = File.read("#{GEM_DIR}/mock/#{filename}")
-        OpenNebula::XMLElement.new(OpenNebula::XMLElement.build_xml(xml, 'VM'))
+
+      let(:history) do
+        history = []
+        rec = {}
+        rec['start_time'] = '1383741169'
+        rec['end_time'] = '1383741259'
+        rec['rstart_time'] = '1383741278'
+        rec['rend_time'] = '1383741367'
+        rec['seq'] = '0'
+        rec['hostname'] = 'supermachine1.somewhere.com'
+        history << rec
+        rec = {}
+        rec['start_time'] = '1383741589'
+        rec['end_time'] = '1383742270'
+        rec['rstart_time'] = '1383741674'
+        rec['rend_time'] = '1383742270'
+        rec['seq'] = '1'
+        rec['hostname'] = 'supermachine2.somewhere.com'
+        history << rec
+
+        history
       end
 
-      context 'vm without RSTIME' do
-        let(:filename) { 'one_worker_RSTIME_missing.xml' }
+      let(:vm_id) { 42 }
+      let(:completed) { true }
 
+      context 'vm without history records' do
         it 'returns 0' do
-          expect(subject.sum_rstime(vm)).to eq(0)
+          expect(subject.sum_rstime([], completed, vm_id)).to eq(0)
         end
       end
 
-      context 'vm with RSTIME that is 0' do
-        let(:filename) { 'one_worker_RSTIME_0.xml' }
-
-        it 'returns 0' do
-          expect(subject.sum_rstime(vm)).to eq(0)
-        end
-      end
-
-      context 'vm without RETIME' do
-        let(:filename) { 'one_worker_RETIME_missing.xml' }
-
-        it 'returns 0' do
-          expect(subject.sum_rstime(vm)).to eq(0)
-        end
-      end
-
-      context 'vm with RETIME that is 0' do
+      context 'vm without rstart_time' do
         before :example do
-          allow(Time).to receive(:now) { 1383741716 }
+          history[0]['rstart_time'] = nil
         end
 
-        let(:filename) { 'one_worker_RETIME_0.xml' }
-
-        it 'returns difference between current time and start of the virtual machine' do
-          expect(subject.sum_rstime(vm)).to eq(42)
+        it 'skips invalid history record and count the rest' do
+          expect(subject.sum_rstime(history, completed, vm_id)).to eq(596)
         end
       end
 
-      context 'vm with RSTIME bigger than RETIME' do
-        let(:filename) { 'one_worker_RSTIME_>_RETIME.xml' }
+      context 'vm with rstart_time that is 0' do
+        before :example do
+          history[0]['rstart_time'] = '0'
+        end
 
-        it 'returns nil' do
-          expect(subject.sum_rstime(vm)).to be_nil
+        it 'skips invalid history record and count the rest' do
+          expect(subject.sum_rstime(history, completed, vm_id)).to eq(596)
+        end
+      end
+
+      context 'vm without rend_time' do
+        before :example do
+          history[0]['rend_time'] = nil
+        end
+
+        it 'skips invalid history record and count the rest' do
+          expect(subject.sum_rstime(history, completed, vm_id)).to eq(596)
+        end
+      end
+
+      context 'vm with rend_time that is 0' do
+        before :example do
+          history[0]['rend_time'] = '0'
+        end
+
+        context 'and running' do
+          before :example do
+            allow(Time).to receive(:now) { 1383741716 }
+          end
+
+          let(:completed) { false }
+
+          it 'returns difference between current time and start of the virtual machine and count the rest' do
+            expect(subject.sum_rstime(history, completed, vm_id)).to eq(1034)
+          end
+        end
+
+        context 'and stopped' do
+          it 'fails with ValidationError' do
+            expect { subject.sum_rstime(history, completed, vm_id) }.to raise_error(Errors::ValidationError)
+          end
+        end
+      end
+
+      context 'vm with rstart_time bigger than rend_time' do
+        before :example do
+          history[0]['rstart_time'] = '1383741259'
+          history[0]['rend_time'] = '1383741169'
+        end
+
+        it 'fails with ValidationError' do
+          expect { subject.sum_rstime(history, completed, vm_id) }.to raise_error(Errors::ValidationError)
+        end
+      end
+
+      context 'with all data valid' do
+        it 'count the correct value' do
+          expect(subject.sum_rstime(history, completed, vm_id)).to eq(685)
         end
       end
     end
